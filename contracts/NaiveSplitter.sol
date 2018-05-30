cee1b4a14256586c11b95a5175a1404a0329e17c
Rob Hitchens <26111999+rob-Hitchens@users.noreply.github.com>
2018-05-30 17:28:10 -0600

    Update NaiveSplitter.sol
    Unfinished ...

--- contracts/NaiveSplitter.sol
+++ contracts/NaiveSplitter.sol
@@ -1,10 +1,19 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.21; 
 
 contract NaiveSplitter {
-
-  function splitFunds(address recipient1, address recipient2) public payable returns (bool success) {
-    recipient1.transfer(msg.value / 2);
-    recipient2.transfer(msg.value / 2);
-    return true;
-  }
+    
+    mapping(address=>uint) public balances; // this will hold the amount of wei owed to any/all addresses
+    
+    function splitFunds(address recipient1, address recipient2) public payable returns(bool success) {
+        uint half = msg.value / 2;
+        balances[recipient1] += half;
+        balances[recipient2] += half;
+        return true;
+    }
+    
+    function withdrawFunds() public returns(bool success) {
+        require(balances[msg.sender] > 0);
+        // now withdraw the funds and do the accounting so they can't do it twice
+        return true;
+    }
 }
